6edb6eedf2b597315d574e2b2203cabda085810f
Gary <garyjair01@hotmail.com>
2024-06-27 09:20:11 -0500

    Ultimos arreglos

--- back/src/middleware/validateMovieData.js
+++ back/src/middleware/validateMovieData.js
@@ -2,11 +2,17 @@ function validateMovieData (req,res,next) {
 
     const { title, year, director, duration, genre, rate, poster } = req.body;
     if (!title || !year || !director || !duration || !genre || !rate || !poster) {
-        return res.status(400).json({ message: 'Faltan campos por rellenar' });
+        return res.status(400).json({ message: 'Faltan campos por llenar' });
 
-    }else {
-        next();
     }
+    const yearRegex = /^\d{4}$/;
+    if (!yearRegex.test(year)) {
+        return res.status(400).json({ message: 'El año debe ser un número de 4 dígitos' });
+    }
+    if (isNaN(rate) || rate < 0 || rate > 10) {
+        return res.status(400).json({ message: 'La calificación debe ser un número entre 0 y 10' });
+    }
+    next();
 }
 
 module.exports = validateMovieData;
--- back/src/routes/movies.js
+++ back/src/routes/movies.js
@@ -3,8 +3,7 @@ const moviesController = require("../controllers/moviesController");
 const validateMovieData = require("../middleware/validateMovieData");
 const moviesRouter = Router();
 
-moviesRouter.post("/", validateMovieData, moviesController.createMovies);
 moviesRouter.get("/", moviesController.getMovies);
-// moviesRouter.post("/", validateMovieData, moviesController.createMovies);
+moviesRouter.post("/", validateMovieData, moviesController.createMovies);
 
 module.exports = moviesRouter;
