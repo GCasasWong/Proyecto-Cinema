2d404fd5f3853018cbd0b08f2e345fe557b0914f
Gary <garyjair01@hotmail.com>
2025-01-30 10:37:51 -0500

    Proyecto M2-Cinema

--- back/src/services/showMessage.js
+++ back/src/services/showMessage.js
@@ -2,19 +2,11 @@ const Movie = require('../models/Movie');
 
 module.exports = {
     getAllMovies: async () => {
-      try{
         return await Movie.find();
-      } catch (error) {
-        throw error;
-      }
     },
 
     createMovie: async (movie) => { 
-      try{
         const newMovie = await Movie.create(movie);
         return newMovie;
-      }catch(error){
-        throw error;
-      }
     }
 };
